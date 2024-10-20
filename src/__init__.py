f16d059c83aa91423df55cbe24c27599b36d5f30
ChugainovPavel <chugainov12@gmail.com>
2024-10-20 22:02:28 +0500

    add title for app instance

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,14 @@ from src.books.views import book_router
 
 version = "v1"
 
-app = FastAPI(version=version)
-app.include_router(book_router, prefix=f"/api/{version}/books")
+app = FastAPI(
+    title="Bookly",
+    description="a rest api for a book review web service",
+    version=version,
+)
+
+app.include_router(
+    book_router,
+    prefix=f"/api/{version}/books",
+    tags=["books"],
+)

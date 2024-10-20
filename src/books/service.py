ba686fda092f354d53a431fd209bbdd1037de284
ChugainovPavel <chugainov12@gmail.com>
2024-10-21 02:20:05 +0500

    fixed views for books

--- src/books/service.py
+++ src/books/service.py
@@ -10,7 +10,7 @@ class BookService:
         result = await session.exec(statement)
         return result.all()
 
-    async def get_book(self, book_uid: str, session: AsyncSession):
+    async def get_book(self, book_uid: int, session: AsyncSession):
         statement = select(Book).where(Book.uid == book_uid)
         result = await session.exec(statement)
         book = result.first()
@@ -25,7 +25,7 @@ class BookService:
         return new_book
 
 
-    async def update_book(self, book_uid: str, update_data: BookUpdateModel, session: AsyncSession):
+    async def update_book(self, book_uid: int, update_data: BookUpdateModel, session: AsyncSession):
         book_to_update = self.get_book(book_uid, session)
 
         if book_to_update is not None:
@@ -38,7 +38,7 @@ class BookService:
             return None
 
 
-    async def delete_book(self, book_uid: str, session: AsyncSession):
+    async def delete_book(self, book_uid: int, session: AsyncSession):
         book_to_delete = self.get_book(book_uid, session)
         if book_to_delete is not None:
             await session.delete(book_to_delete)

3fffd969ac12570f545548292dca5a59c4fd6e85
ChugainovPavel <chugainov12@gmail.com>
2024-10-22 01:00:11 +0500

    books crud chages

--- src/books/views.py
+++ src/books/views.py
@@ -49,13 +49,15 @@ async def update_book(
         session: AsyncSession = Depends(get_session),
 ):
     updated_book = await book_service.update_book(book_uid, book_update_data, session)
-    if updated_book:
-        return updated_book
-    else:
+    if updated_book is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Book not found",
         )
+
+    else:
+        return updated_book
+
 
 
 @book_router.delete("/{book_uid}", status_code=status.HTTP_204_NO_CONTENT)
@@ -64,14 +66,13 @@ async def delete_book(
         session: AsyncSession = Depends(get_session),
 ):
     book_to_delete = await book_service.delete_book(book_uid, session)
-    if book_to_delete:
-        return None
-    else:
+    if book_to_delete is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Book not found",
         )
-
+    else:
+        return {}
 
 
 

b72994d1a6fe94764e06c5c596aca8a7eca04b29
ChugainovPavel <chugainov12@gmail.com>
2024-10-23 01:14:24 +0500

    Created acces and refresh tokens, created views for login

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
-from .schemas import UserCreateModel, UserModel
+from fastapi.responses import JSONResponse
+from .schemas import UserCreateModel, UserModel, UserLoginModel
 from .service import UserService
 from src.db.main import get_session
 from sqlmodel.ext.asyncio.session import AsyncSession
-from .utils import create_access_token, decode_token
+from .utils import create_access_token, decode_token, verify_password
+from datetime import timedelta
 
 
 auth_router = APIRouter()
 user_service = UserService()
+
+REFRESH_TOKEN_EXPIRE = 2
 
 
 @auth_router.post(
@@ -33,5 +37,43 @@ async def create_user_Account(
 
 
 @auth_router.post("/login")
-async def login_users():
-    pass
+async def login_users(login_data: UserLoginModel, session: AsyncSession = Depends(get_session)):
+    email = login_data.email
+    password = login_data.password
+    user = await user_service.get_user_by_email(email, session)
+
+    if user is not None:
+        password_valid = verify_password(password, user.password_hash)
+
+        if password_valid:
+            access_token = create_access_token(
+                user_data={
+                    "email": user.email,
+                    "user_uuid": str(user.uid)
+                }
+            )
+            refresh_token = create_access_token(
+                user_data={
+                    "email": user.email,
+                    "user_uuid": str(user.uid)
+                },
+                refresh=True,
+                expire=timedelta(days=REFRESH_TOKEN_EXPIRE),
+            )
+
+            return JSONResponse(
+                content= {
+                    "message": "login successfully",
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "user": {
+                        "email": user.email,
+                        "uid": str(user.uid),
+                    }
+                }
+            )
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid email or password",
+    )
